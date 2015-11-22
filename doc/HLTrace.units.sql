60d545c20cee65c2fdc19490efbabc75015f0b93
Piotr Wilkosz <piotr.wilkosz@gmail.com>
2015-11-23 00:44:41 +0100

    Add doc with default values for status_types, operation_status and units. Update data.

---
+++ doc/HLTrace.units.sql
@@ -0,0 +1,7 @@
+INSERT OR REPLACE INTO unit VALUES (0, "None", "None", "" );
+INSERT OR REPLACE INTO unit VALUES (1, "Newton", "N", "" );
+INSERT OR REPLACE INTO unit VALUES (2, "Newton metre", "Nm", "" );
+INSERT OR REPLACE INTO unit VALUES (3, "Angle (degree)", "deg", "" );
+INSERT OR REPLACE INTO unit VALUES (4, "Distance", "mm", "" );
+INSERT OR REPLACE INTO unit VALUES (5, "Kilo Newton", "kN", "" );
+INSERT OR REPLACE INTO unit VALUES (6, "Pascal", "Pa", "" );
